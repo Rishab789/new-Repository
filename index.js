49e5c05ef22543980f3eaf7689ec943d120e566c
Rishab789 <beraprakash456@gmail.com>
2023-07-22 19:24:09 +0530

    hello

--- index.js
+++ index.js
@@ -1,17 +1,125 @@
-function sendEmail(){
-    Email.send({
-        Host : "smtp.gmail.com",
-        Username : "beraprakash456@gmail.com",
-        Password : "Rourkela@890",
-        To : 'beraprakash456@gmail.com',
-        From : document.getElementById("email").value,
-        Subject : "Nwe Contact Form Enquiry",
-        Body : "Name:"+ document.getElementById('name').value
-               + "<br> Email: " + document.getElementById('email').value
-               + "<br> Phone no.: " + document.getElementById('phone').value
-               + "<br> Message: " + document.getElementById('message').value
-    }).then(
-      message => alert("Message Sent Successfully")
-    );
+ const home = document.getElementById("Home");
+ const services = document.getElementById("Services");
+ const aboutus = document.getElementById("About");
+ const contact = document.getElementById("Contact");
+
+
+ home.addEventListener("click" , ()=>{
+  home.classList.add("active");
+  services.classList.remove("active");
+  aboutus.classList.remove("active");
+  contact.classList.remove("active");
+
+  
+
+ });
+
+ services.addEventListener("click" , ()=>{
+  services.classList.add("active");
+  aboutus.classList.remove("active");
+  contact.classList.remove("active");
+  home.classList.remove("active");
+
+
+ });
+
+ aboutus.addEventListener("click" , ()=>{
+  aboutus.classList.add("active");
+  contact.classList.remove("active");
+  home.classList.remove("active");
+  services.classList.remove("active");
+
+
+ });
+
+ contact.addEventListener("click" , ()=>{
+  contact.classList.add("active");
+  home.classList.remove("active");
+  services.classList.remove("active");
+  aboutus.classList.remove("active");
+
+ });
+
+  //  const topButton = document.querySelector(".top-contact-btn");
+  //  topButton.addEventListener("click" ,  sendMail);
+
+//  const lowButton = document.querySelector(".btn");
+//  lowButton.addEventListener("click" , sendMail);
+
+
+
+
+
+ 
+function sendMail(isTop = false){
+ 
+  var params = {
+    from_name: isTop ? document.getElementById('fullName_top').value : document.getElementById('fullName').value,
+    email_id:  isTop ? document.getElementById('email_id_top').value : document.getElementById('email_id').value,
+    phone:  isTop ? document.getElementById('phone_top').value : document.getElementById('phone').value,
+    message:  isTop ? document.getElementById('message_top').value : document.getElementById('message').value
+   
+  };
+
+  
+  if(params.from_name ==="" || params.email_id==="" || params.phone==="" || params.message === ""){
+    alert("please fill the required fields.");
     
+  }
+  
+  else {
+     emailjs.send("service_0ci8bji" , "template_pqsumrd" , params).then(function(res){
+      alert("message sent successfully");  
+   
+     isTop ? document.getElementById('fullName_top').value = "" : document.getElementById('fullName').value = "",
+     isTop ? document.getElementById('email_id_top').value = "": document.getElementById('email_id').value ="",
+     isTop ? document.getElementById('phone_top').value = "" : document.getElementById('phone').value = "",
+     isTop ? document.getElementById('message_top').value = "" : document.getElementById('message').value= ""
+      
+      
+    });
+   
+  }
 }
+
+function myfun(){
+  var phoneCheck = document.querySelector(".phoneCheck").value;
+  var pattern = /^[0-9]{9,11}$/;
+  if(pattern.test(phoneCheck)){
+    document.getElementById("msg").innerHTML="Valid Password";
+    sendMail();
+
+  }else{
+
+    document.getElementById("msg").innerHTML="Invalid Password";
+    return false;
+    
+  }
+}
+
+
+  
+
+const serviceBtn = document.querySelector(".serviceBtn");
+const btnP = document.querySelector(".btnP");
+
+function serviceButton(){
+  window.location="http://127.0.0.1:5500/services.html";
+
+}
+
+
+function homebtn(){
+  window.location="http://127.0.0.1:5500/index.html";
+}
+
+
+const redBtn = document.querySelector(".redBtn");
+function redButton(){
+  window.location="http://127.0.0.1:5500/index.html#contact";
+}
+ function blackButton(){
+  window.location="http://127.0.0.1:5500/index.html";
+
+ }
+
